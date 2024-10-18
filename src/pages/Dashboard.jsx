1f226d9199f9f7fb685775af0c18a61f248e1098
shreyasepam <shreyas_bm@epam.com>
2024-10-18 10:17:12 +0530

    css resize for dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -10,15 +10,15 @@ export default function Dashboard() {
   return (
     <div className='flex flex-col items-center'>
       <h1 className='font-bold mt-4 text-lg'>Dashboard</h1>
-      <div>
+      <div className='w-full md:w-3/6'>
         <button
-          className="focus:shadow-outline mt-4 w-80 transform rounded border border-gray-100 bg-white py-2 px-4 font-semibold text-gray-800 shadow transition duration-150 ease-in-out focus:outline-none active:scale-95 active:bg-gray-200"
+          className="focus:shadow-outline mt-4 w-3/6 transform rounded border border-gray-100 bg-white py-2 px-4 font-semibold text-gray-800 shadow transition duration-150 ease-in-out focus:outline-none active:scale-95 active:bg-gray-200"
           onClick={() => onNavigateHandle("profile")}
         >
           Profile
         </button>
         <button
-          className="focus:shadow-outline mt-4 w-80 transform rounded border border-gray-100 bg-white py-2 px-4 font-semibold text-gray-800 shadow transition duration-150 ease-in-out focus:outline-none active:scale-95 active:bg-gray-200"
+          className="focus:shadow-outline mt-4 w-3/6 transform rounded border border-gray-100 bg-white py-2 px-4 font-semibold text-gray-800 shadow transition duration-150 ease-in-out focus:outline-none active:scale-95 active:bg-gray-200"
           onClick={() => onNavigateHandle("settings")}
         >
           Settings
